5bd2d7ee93782f07edd920ddad189dcf552e6cd4
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-03-15 17:22:09 -0500

    fix: add types for verify function in oauth strategy

--- apps/api-rest/src/main.ts
+++ apps/api-rest/src/main.ts
@@ -8,7 +8,14 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.use(express.json());
-app.use(session({ secret: 'my-secret' }));
+app.use(
+  session({
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true },
+  })
+);
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
--- libs/api-rest/auth/src/lib/strategies/oauth-google.ts
+++ libs/api-rest/auth/src/lib/strategies/oauth-google.ts
@@ -1,4 +1,4 @@
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as GoogleStrategy, Profile } from 'passport-google-oauth20';
 import {
   createFederatedCredentials,
   createUser,
@@ -7,6 +7,9 @@ import {
   findUserById,
 } from '../data-access';
 import crypto from 'crypto';
+import { users } from '@generalist/db';
+
+type User = typeof users.$inferSelect;
 
 const oauthGoogle = new GoogleStrategy(
   {
@@ -16,33 +19,40 @@ const oauthGoogle = new GoogleStrategy(
     scope: ['profile', 'email'],
     state: true,
   },
-  async function verify(_accessToken, _refreshToken, profile, cb) {
+  async function verify(
+    _accessToken: string,
+    _refreshToken: string,
+    profile: Profile,
+    cb: (error: Error | null, user?: User) => void
+  ) {
     const credentials = await findFederatedCredentailsByProviderAndSubject({
       provider: 'https://accounts.google.com',
       subject: profile.id,
     });
     if (!credentials) {
-      const user = await findUserByEmail(profile.emails[0].value);
+      const userEmail = profile.emails?.[0].value;
+      if (!userEmail) {
+        return cb(new Error('User email not found'));
+      }
+      const user = await findUserByEmail(userEmail);
       if (user) {
         return cb(new Error('User have another authentication method'));
       }
       const newUser = await createUser({
         id: crypto.randomUUID(),
         name: profile.displayName,
-        email: profile.emails[0].value,
+        email: userEmail,
       });
 
       if (!newUser) {
         return cb(new Error('Error creating user'));
       }
-
-      const newFederatedCredentials = createFederatedCredentials({
+      const newFederatedCredentials = await createFederatedCredentials({
         id: crypto.randomUUID(),
         userId: newUser.id,
         provider: 'https://accounts.google.com',
         subject: profile.id,
       });
-
       if (!newFederatedCredentials) {
         return cb(new Error('Error creating federated credentials'));
       }

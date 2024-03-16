283ca3c390adbac5885a56717807bbf0c9b97291
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-03-15 21:23:46 -0500

    fix: do not use type safety env variables for e2e tests for now

--- apps/api-rest-e2e/src/support/test-setup.ts
+++ apps/api-rest-e2e/src/support/test-setup.ts
@@ -1,11 +1,10 @@
 /* eslint-disable */
 
 import axios from 'axios';
-import { env } from '@generalist/api-rest/env';
 
 module.exports = async function () {
   // Configure axios for tests to use.
-  const host = env.HOST ?? 'localhost';
-  const port = env.PORT ?? '3000';
+  const host = process.env.HOST ?? 'localhost';
+  const port = process.env.PORT ?? '3000';
   axios.defaults.baseURL = `http://${host}:${port}`;
 };

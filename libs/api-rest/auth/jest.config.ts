34f536144b25dbc5dadc4927d0a9312659b57deb
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-03-16 20:43:33 -0500

    chore: pass jest suite if there are not tests

--- libs/api-rest/auth/jest.config.ts
+++ libs/api-rest/auth/jest.config.ts
@@ -3,6 +3,7 @@ export default {
   displayName: 'api-rest-auth',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
+  passWithNoTests: true,
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },

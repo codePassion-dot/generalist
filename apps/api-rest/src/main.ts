a93272362c3b0d8cd2d686bc81b0fbbf658a5ed2
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-03-26 19:22:29 -0500

    fix: use cookie secure option only in production

--- apps/api-rest/src/main.ts
+++ apps/api-rest/src/main.ts
@@ -8,15 +8,19 @@ const host = env.HOST ?? 'localhost';
 const port = env.PORT ? Number(env.PORT) : 3000;
 const app = express();
 
+const sessionOpts: session.SessionOptions = {
+  secret: 'my-secret',
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  sessionOpts.cookie.secure = true; // serve secure cookies
+}
+
 app.use(express.json());
-app.use(
-  session({
-    secret: 'my-secret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: true },
-  })
-);
+app.use(session(sessionOpts));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });

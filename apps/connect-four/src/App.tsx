67f7ffa874047251c1b2cbb81a7a01a15cf342c1
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2024-04-14 18:56:27 -0500

    chore(connect-four): add solid-devtools runtime entry point

--- apps/connect-four/src/App.tsx
+++ apps/connect-four/src/App.tsx
@@ -1,5 +1,6 @@
 import { GameBoardProvider } from "@generalist/connect-four/game-board";
 import { ParentComponent } from "solid-js";
+import "solid-devtools";
 
 const App: ParentComponent = (props) => {
   return <GameBoardProvider>{props.children}</GameBoardProvider>;
